d714cc56b2cc1a5732ae7b120c681487448dc87f
bora <shearlonil@gmail.com>
2025-04-06 03:18:17 +0100

    event update completed

--- api-services/event-service.js
+++ api-services/event-service.js
@@ -6,12 +6,24 @@ const sequelize = db.sequelize;
 
 const findById = async id => {
     return await Event.findByPk(id, {
-        attributes: ['id', 'value', 'mark_done']
+        include: [
+            {
+                model: EventDates,
+            },
+        ],
     });
 };
 
 const create = async event => {
     try {
+        const count = await Event.count({
+            where: {
+                mark_done: false,
+            },
+        });
+        if (count >= 6) {
+            throw new Error("Limit Reached. Max of 6 events allowed. Consider Marking some events as done to add new events");
+        }
         await sequelize.transaction(async (t) => {
             const { title, venue, time, dates } = event;
             const e = await Event.create({ title, venue, time, mark_done: false }, { transaction: t });
@@ -30,6 +42,44 @@ const create = async event => {
         throw new Error(error.message); // rethrow the error for front-end 
     }
     // await Event.create({ value: jsonData, mark_done: false });
+};
+
+const update = async event => {
+    try {
+        await sequelize.transaction(async (t) => {
+            const { id, title, venue, time, dates } = event;
+            const e = await Event.findByPk(id);
+            e.title = title;
+            e.venue = venue;
+            e.time = time;
+
+            await e.save();
+
+            //  delete all previous date entries to pave way for new dates
+            await EventDates.destroy({ where: { event_id: id }, transaction: t });
+            
+            //  sequelize transaction not working with forEach loop
+            //  https://stackoverflow.com/questions/61369310/sequelize-transactions-inside-foreach-issue
+            for (const date of dates) {
+                await e.createEventDate({ date }, { transaction: t });
+            }
+        });
+        //  If the execution reaches this line, the transaction has been committed successfully
+        //  result` is whatever was returned from the transaction callback (the `user`, in this case)
+    } catch (error) {
+        //  If the execution reaches this line, an error occurred.
+        //  The transaction has already been rolled back automatically by Sequelize!
+        throw new Error(error.message); // rethrow the error for front-end 
+    }
+    // await Event.create({ value: jsonData, mark_done: false });
+};
+
+const markdone = async id => {
+    await Event.update({ mark_done: true }, {
+        where: {
+            id,
+        },
+    });
 };
 
 const recent = async (pageSize) => {
@@ -62,6 +112,8 @@ const upcoming = async (pageSize) => {
 module.exports = {
     findById,
     create,
+    update,
+    markdone,
     recent,
     upcoming,
 };
--- controllers/event-controller.js
+++ controllers/event-controller.js
@@ -5,11 +5,39 @@ const { verifyAccessToken } = require('../middleware/jwt');
 const validate = require('../middleware/schemer-validator');
 const { schema } = require('../schema/yup-schemas/event-schema');
 const eventService = require('../api-services/event-service');
+const { routePositiveNumberMiscParamSchema } = require('../schema/yup-schemas/request-params');
 
 const create = async (req, res) => {
     try {
         await eventService.create(req.body);
         res.sendStatus(200);
+    } catch (error) {
+        return res.status(400).json({'message': error.message});
+    }
+};
+
+const update = async (req, res) => {
+    try {
+        await eventService.update(req.body);
+        res.sendStatus(200);
+    } catch (error) {
+        return res.status(400).json({'message': error.message});
+    }
+};
+
+const markdone = async (req, res) => {
+    try {
+        routePositiveNumberMiscParamSchema.validateSync(req.params.id);
+        return res.status(200).json(await eventService.markdone(req.params.id));
+    } catch (error) {
+        return res.status(400).json({'message': error.message});
+    }
+};
+
+const findById = async (req, res) => {
+    try {
+        routePositiveNumberMiscParamSchema.validateSync(req.params.id);
+        return res.status(200).json(await eventService.findById(req.params.id));
     } catch (error) {
         return res.status(400).json({'message': error.message});
     }
@@ -24,6 +52,9 @@ const recentEvents = async (req, res) => {
 };
 
 router.route('/create').post( verifyAccessToken, validate(schema), create );
+router.route('/update').post( verifyAccessToken, validate(schema), update );
+router.route('/update/:id').post( verifyAccessToken, markdone );
+router.route('/find/:id').get( verifyAccessToken, findById );
 router.route('/upcoming').get( upcoming );
 router.route('/recent').get( recentEvents );
 
